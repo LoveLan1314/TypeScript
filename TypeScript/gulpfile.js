89499182e26415053d70aecb4252b2ac5bc7d86d
LoveLan1314 <736265181@qq.com>
2018-06-22 01:21:37 +0800

    6.22

--- TypeScript/gulpfile.js
+++ TypeScript/gulpfile.js
@@ -7,7 +7,15 @@ var del = require('del');
 
 var paths = {
     scripts: ['scripts/**/*.js', 'scripts/**/*.ts', 'scripts/**/*. map'],
+    libs: ['node_modules/angular2/bundles/angular2.js',
+        'node_modules/angular2/bundles/angular2-polyfills.js',
+        'node_modules/systemjs/dist/system.src.js',
+        'node_modules/rxjs/Rx.js']
 };
+
+gulp.task('lib', function () {
+    gulp.src(paths.libs).pipe(gulp.dest('wwwroot/scripts/lib'));
+});
 
 gulp.task('clean', function () {
     return del(['wwwroot/scripts/**/*']);

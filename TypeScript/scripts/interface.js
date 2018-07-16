a59001f31c2fd9157e1bd4a09c9fa3a87c055a5a
LoveLan1314 <736265181@qq.com>
2018-07-16 23:55:11 +0800

    7.16

--- TypeScript/scripts/interface.js
+++ TypeScript/scripts/interface.js
@@ -80,4 +80,18 @@ var AnalogClock = (function () {
 }());
 var digital = createClock(DigitalClock, 12, 17);
 var analog = createClock(AnalogClock, 7, 32);
+var square = {};
+square.color = "blue";
+square.sideLength = 10;
+square.penWidth = 5.0;
+function getCounter() {
+    var counter = function (start) { };
+    counter.interval = 123;
+    counter.reset = function () { };
+    return counter;
+}
+var c = getCounter();
+c(10);
+c.reset();
+c.interval = 5.0;
 //# sourceMappingURL=interface.js.map

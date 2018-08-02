d3680c6e9da42e31f693c1d13931f5b1b2136a16
LoveLan1314 <736265181@qq.com>
2018-08-02 22:38:30 +0800

    类型推断

---
+++ TypeScript/scripts/type-inference.js
@@ -0,0 +1,5 @@
+var x1 = [0, 1, null];
+window.onmousedown = function (mouseEvent) {
+    console.log(mouseEvent.button);
+};
+//# sourceMappingURL=type-inference.js.map
---
+++ TypeScript/scripts/type-inference.ts
@@ -0,0 +1,5 @@
+﻿let x1 = [0, 1, null];
+
+window.onmousedown = function (mouseEvent) {
+    console.log(mouseEvent.button);
+}

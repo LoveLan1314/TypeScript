9d6b01b45a9d1f8bc5c201acd83dcd7e4bbae06a
LoveLan1314 <736265181@qq.com>
2018-08-10 21:51:27 +0800

    Symbols

--- TypeScript/scripts/symbols.js
+++ TypeScript/scripts/symbols.js
@@ -1,8 +1,54 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var collection_1 = require("angular2/src/facade/collection");
 var Symbols;
 (function (Symbols) {
     var sym1 = Symbol();
     var sym2 = Symbol("key");
     var sym3 = Symbol("key");
     sym2 === sym3; // false, symbols是唯一的
+    var sym = Symbol();
+    var obj = (_a = {},
+        _a[sym] = "value",
+        _a);
+    console.log(obj[sym]); // "value"
+    var getClassNameSymbol = Symbol();
+    var C = /** @class */ (function () {
+        function C() {
+        }
+        C.prototype[getClassNameSymbol] = function () {
+            return "C";
+        };
+        return C;
+    }());
+    var c = new C();
+    var className = c[getClassNameSymbol](); // "C"
+    var someArray = [1, "string", false];
+    for (var _i = 0, someArray_1 = someArray; _i < someArray_1.length; _i++) {
+        var entry = someArray_1[_i];
+        console.log(entry); // 1, "string", false
+    }
+    var list = [4, 5, 6];
+    for (var i in list) {
+        console.log(i); // "0", "1", "2"
+    }
+    for (var _b = 0, list_1 = list; _b < list_1.length; _b++) {
+        var i = list_1[_b];
+        console.log(i); // "4", "5", "6"
+    }
+    var pets = new collection_1.Set(["Cat", "Dog", "Hamster"]);
+    //pets["species"] = "mammals";
+    for (var pet in pets) {
+        console.log(pet); // "species"
+    }
+    for (var pet in pets) {
+        console.log(pet); // "Cat", "Dog", "Hamster"
+    }
+    var numbers = [1, 2, 3];
+    for (var _c = 0, numbers_1 = numbers; _c < numbers_1.length; _c++) {
+        var num = numbers_1[_c];
+        console.log(num);
+    }
+    var _a;
 })(Symbols || (Symbols = {}));
 //# sourceMappingURL=symbols.js.map
--- TypeScript/scripts/symbols.ts
+++ TypeScript/scripts/symbols.ts
@@ -1,4 +1,6 @@
-﻿namespace Symbols {
+﻿import { Set } from "angular2/src/facade/collection";
+
+namespace Symbols {
     let sym1 = Symbol();
 
     let sym2 = Symbol("key");
@@ -6,5 +8,55 @@
     let sym3 = Symbol("key");
 
     sym2 === sym3;  // false, symbols是唯一的
-    
+
+    let sym = Symbol();
+
+    let obj = {
+        [sym]: "value"
+    };
+
+    console.log(obj[sym]);  // "value"
+
+    const getClassNameSymbol = Symbol();
+
+    class C {
+        [getClassNameSymbol]() {
+            return "C";
+        }
+    }
+
+    let c = new C();
+    let className = c[getClassNameSymbol]();    // "C"
+
+    let someArray = [1, "string", false];
+
+    for (let entry of someArray) {
+        console.log(entry); // 1, "string", false
+    }
+
+    let list = [4,5,6];
+
+    for (let i in list) {
+        console.log(i); // "0", "1", "2"
+    }
+
+    for (let i of list) {
+        console.log(i); // "4", "5", "6"
+    }
+
+    let pets = new Set(["Cat", "Dog", "Hamster"]);
+    //pets["species"] = "mammals";
+
+    for (let pet in pets) {
+        console.log(pet);   // "species"
+    }
+
+    for (let pet in pets) {
+        console.log(pet);   // "Cat", "Dog", "Hamster"
+    }
+
+    let numbers = [1, 2, 3];
+    for (let num of numbers) {
+        console.log(num);
+    }
 }
